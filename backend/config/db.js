7fd844e9385a7b8272f84abaad3eccdcf5a8b616
unknown <dwiprajdey33720@gmail.com>
2026-03-14 22:39:58 +0530

    mongo atlas

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_Compass);
+    const conn = await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(`▶️  MongoDB connected`);
   } catch (error) {
     console.log(`Error: ${error.message}`);
