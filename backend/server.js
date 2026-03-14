6b4c45a7ca9c67f4c29e62bdc52dff29779a3360
unknown <dwiprajdey33720@gmail.com>
2026-03-14 22:02:44 +0530

    cros fixing

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import cors from 'cors';    
+import express from "express";
+import cors from "cors";
 import "dotenv/config";
-import { connectDB } from './config/db.js';
-import userRoutes from './routes/userRotes.js';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import resumeRouter from './routes/resumeRoutes.js';
+import { connectDB } from "./config/db.js";
+import userRoutes from "./routes/userRotes.js";
+import resumeRouter from "./routes/resumeRoutes.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,28 +13,33 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 connectDB();
 
 app.use(express.json());
 
-app.use("/api/auth",userRoutes)
-app.use("/api/resume",resumeRouter)
+app.use("/api/auth", userRoutes);
+app.use("/api/resume", resumeRouter);
 
-app.use("/uploads",
-    express.static(path.join(__dirname,"uploads"),{
-        setHeaders:(req,_path)=>{
-            res.set("Access-Control-Allow-Origin",`http://localhost:5173`);
-        }
-    })
-)
- 
+app.use(
+  "/uploads",
+  express.static(path.join(__dirname, "uploads"), {
+    setHeaders: (res, path) => {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+    },
+  })
+);
 
-app.get('/', (req, res) => {
-    res.send('API is running...');
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
