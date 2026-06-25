fa7c99e26c75d879ea7c83caa44dd11a2d6328f6
unknown <dwiprajdey33720@gmail.com>
2026-06-25 17:48:14 +0530

    adding the frontend url

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173" || "https://resume-builder-h217.vercel.app/"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   })
