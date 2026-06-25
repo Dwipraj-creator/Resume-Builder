50b34bff7fe2b80003dae44bd4af95bb0baac05d
unknown <dwiprajdey33720@gmail.com>
2026-06-25 17:51:46 +0530

    fixing the url error

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173" || "https://resume-builder-h217.vercel.app/"],
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    origin: [
+      "http://localhost:5173",
+      "https://resume-builder-h217.vercel.app"
+    ],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
